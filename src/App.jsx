52218d3902daca3312450a335fd9a2b626fdb0bd
Choyon-Dhor <choyon.dhr@gmail.com>
2026-02-26 03:23:13 +0600

    Update dependencies and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import LeftSocialBar from './components/LeftSocialBar'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Quote from './components/Quote'
 import Projects from './components/Projects'
-import Skills from './components/Skills'              // Original inline skills
-import SkillsProgress from './components/SkillProgress' // Fixed filename
+import Skills from './components/Skills'              
+import SkillsProgress from './components/SkillProgress' 
 import About from './components/About'
 import Activities from './components/Activities'
 import Contact from './components/Contact'
@@ -31,7 +32,7 @@ function HomePage() {
         <Separator />
         <Projects />
         <Separator />
-        {/* If you want both skills sections, keep both; otherwise remove one */}
+        
         {/*<Skills /> */}
         <SkillsProgress />
         <Separator />
@@ -51,6 +52,7 @@ function HomePage() {
 function App() {
   return (
     <BrowserRouter>
+    <Analytics />
     <LeftSocialBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
